8311189db5a37fd2b82a38bc3670fd2b401fcd9c
Joni <t0sejo01@students.oamk.fi>
2021-11-02 17:06:17 +0200

    new code added

--- javaproject/src/main/java/joni/java/javaproject/ICourseFileService.java
+++ javaproject/src/main/java/joni/java/javaproject/ICourseFileService.java
@@ -1,5 +1,12 @@
 package joni.java.javaproject;
 
-public class ICourseFileService {
+import java.io.FileNotFoundException;
+import java.util.List;
+import joni.java.javaproject.Models.Course;
+import joni.java.javaproject.Models.Student;
+
+public interface ICourseFileService {
     
+    List<Student> readStudentsFromFile(String filePath) throws FileNotFoundException;
+    List<Course> readCoursesFromFile(String filePath) throws FileNotFoundException;
 }
--- javaproject/src/main/java/joni/java/javaproject/ICourseService.java
+++ javaproject/src/main/java/joni/java/javaproject/ICourseService.java
@@ -1,5 +1,15 @@
 package joni.java.javaproject;
 
-public class ICourseService {
-    
+import java.util.List;
+import joni.java.javaproject.Models.Course;
+import joni.java.javaproject.Models.Student;
+
+public interface ICourseService {
+     
+    List<Student> getStudents();
+    List<Course> getCOurses();
+    Student getStudentById(long studentId);
+    Course getCourseById(long courseId);
+    List<Course> getCoursesOfStudent(long studentId);
+    boolean addStudentToCourse(long studentId, long courseId);
 }
