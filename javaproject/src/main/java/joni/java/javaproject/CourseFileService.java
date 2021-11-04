dda91dbde2c5e3bec9460c2ca4044cdbb4aa589f
Joni <t0sejo01@students.oamk.fi>
2021-11-04 15:10:34 +0200

    adding new code

--- javaproject/src/main/java/joni/java/javaproject/CourseFileService.java
+++ javaproject/src/main/java/joni/java/javaproject/CourseFileService.java
@@ -1,5 +1,52 @@
 package joni.java.javaproject;
 
-public class CourseFileService {
-    
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import joni.java.javaproject.Models.Course;
+import joni.java.javaproject.Models.LocalCourse;
+import joni.java.javaproject.Models.OnlineCourse;
+import joni.java.javaproject.Models.Student;
+
+public class CourseFileService implements ICourseFileService {
+
+    @Override
+    public List<Student> readStudentsFromFile(String filePath) throws FileNotFoundException {
+        File file = new File(filePath);
+        List<Student> studentsFromFile = new ArrayList<>();
+        Scanner sc = new Scanner(file);
+
+        while(sc.hasNext()) {
+            String[] rowData = sc.nextLine().split(" ");
+            studentsFromFile.add(new Student(rowData[0], rowData[1]));
+        }
+        sc.close();
+        return studentsFromFile;
+    }
+
+    @Override
+    public List<Course> readCoursesFromFile(String filePath) throws FileNotFoundException {
+        File file = new File(filePath);
+        List<Course> coursesFromFile = new ArrayList<>();
+        Scanner sc = new Scanner(file);
+
+        while(sc.hasNext()) {
+            String[] rowData = sc.nextLine().split("--");
+
+            if(rowData.length > 3){
+            coursesFromFile.add(new OnlineCourse(rowData[0], rowData[1], new ArrayList<>(), rowData[2]));
+            }
+            else{
+                coursesFromFile.add(new LocalCourse(rowData[0], rowData[1], new ArrayList<>(), rowData[2]));
+
+            }
+        }
+        sc.close();
+        return coursesFromFile;
+    }
 }
+    
+
--- javaproject/src/main/java/joni/java/javaproject/CourseService.java
+++ javaproject/src/main/java/joni/java/javaproject/CourseService.java
@@ -1,13 +1,34 @@
 package joni.java.javaproject;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import java.util.ArrayList;
+import java.util.List;
 
-import antlr.collections.List;
 import joni.java.javaproject.Models.Course;
+import joni.java.javaproject.Models.Student;
 
-@RestController
-public class CourseService {
+public class CourseService  implements ICourseService{
+
+    List<Student> students = new ArrayList<>();
+    List<Course> courses = new ArrayList<>();
+    CourseFileService fileService = new CourseFileService();
+
+    public CourseService() {
+
+        try {
+            this.students = fileService.readStudentsFromFile("students.txt");
+        } catch (Exception e) {
+            e.printStackTrace(); 
+        }
+
+        try {
+            this.courses = fileService.readCoursesFromFile("courses.txt");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+   
+
+    
     
 
 }
